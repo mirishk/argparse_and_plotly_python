f9b16d1a228637c7d651e2d2b8b39651199acbe6
mirishk <135973843+mirishk@users.noreply.github.com>
2024-07-28 16:06:58 +0300

    Update viz.py

--- code/viz.py
+++ code/viz.py
@@ -1,70 +1,78 @@
 import plotly.express as px
 import plotly.io as pio  #for html file
 
-# Get the Plotly chart as HTML content
 def _chart_in_html(fig):
+        
+        # Convert a Plotly figure to HTML content.
+        # Returns the HTML content of the figure.
+
         chart = pio.to_html(fig, full_html=False)
         return chart
 
 class Visualizer:
     # 1 ===============================================================
-    # Function that plot bar chart of top N values in specific column
+  
     @staticmethod
     def plot_topN(agg_df,column, topN):
+
+        # Plot a bar chart of top N values in a specific column.
+        # Returns the HTML content of the Plotly figure.
         
-        fig = px.bar(agg_df, x = column, y = 'Count',  
-                    color = column, color_discrete_sequence = ['red'],
-                    text = agg_df['Count']
+        fig = px.bar(
+            agg_df, x = column, y = 'Count',  
+            color = column, color_discrete_sequence = ['red'],
+            text = agg_df['Count']
         )
         
-        if column=='cast':
-            title_val = 'Actor'
-        else:
-            title_val = f'{column.title()}'
+        title_val = 'Actor' if column == 'cast' else column.title()
         
-        fig.update_layout(xaxis_title = title_val, yaxis_title = 'Count', 
-                        plot_bgcolor = 'black', paper_bgcolor = 'black',
-                        font = dict(color='white'),
-                        bargap = 0.5,  # Adjust space between bars
-                        height = 500, width=1000,
-                        title=dict(text=f'Top {topN} {title_val}s', x=0.5)  # Set the title and center it horizontally
+        fig.update_layout(
+            xaxis_title = title_val, yaxis_title = 'Count', 
+            plot_bgcolor = 'black', paper_bgcolor = 'black',
+            font = dict(color='white'),
+            bargap = 0.5,  # Adjust space between bars
+            height = 500, width=1000,
+            title=dict(text=f'Top {topN} {title_val}s', x=0.5)  # Set the title and center it horizontally
         )
 
         return _chart_in_html(fig)
 
     # 2 =======================================================================
-    # Create a line chart
+    
     @staticmethod
     def plot_line_chart(grouped_df, interval):
         
-        # for average line
+        # Create a line chart of count by interval.
+        # Returns the HTML content of the Plotly figure.
+
         average_count = grouped_df.groupby('type')['count'].mean().reset_index(name='average_count')
 
-        fig = px.line(grouped_df, x = interval, y = 'count', color = 'type',
-                title = f'Count of Movies and TV Shows Added Each {interval.capitalize()}',
-                labels = {'year': 'Year', 'month':'Month', 'count': 'Count', 'type': 'Type'},
-                markers=True,
-                color_discrete_sequence = ['#FF0000', '#ff8080']
+        fig = px.line(
+            grouped_df, x = interval, y = 'count', color = 'type',
+            title = f'Count of Movies and TV Shows Added Each {interval.capitalize()}',
+            labels = {'year': 'Year', 'month':'Month', 'count': 'Count', 'type': 'Type'},
+            markers=True,
+            color_discrete_sequence = ['#FF0000', '#ff8080']
         )
 
         # Add average lines for movies and TV shows
         for _, row in average_count.iterrows():
-            fig.add_shape(type = 'line',
-                        x0 = grouped_df[interval].min(),
-                        y0 = row['average_count'],
-                        x1 = grouped_df[interval].max(),
-                        y1 = row['average_count'],
-                        line = dict(color='yellow', dash='dash')
-                        )
-            # Add annotation for the average count value
-            fig.add_annotation(x = grouped_df[interval].max() + 0.5, y = row['average_count'],
-                            text = f'AVG: {row["average_count"]:.2f}',
-                            showarrow = False,
-                            font = dict(color='yellow', size=10)
-                            )
+            fig.add_shape(
+                type = 'line',
+                x0 = grouped_df[interval].min(),
+                y0 = row['average_count'],
+                x1 = grouped_df[interval].max(),
+                y1 = row['average_count'],
+                line = dict(color='yellow', dash='dash')
+            )
+            
+            fig.add_annotation(
+                x = grouped_df[interval].max() + 0.5, y = row['average_count'],
+                text = f'AVG: {row["average_count"]:.2f}',
+                showarrow = False,
+                font = dict(color='yellow', size=10)
+            )
 
-        
-        # Customizing appearance
         fig.update_traces(line=dict(width=2.5), marker=dict(size=10))
         
         fig.update_layout(
@@ -79,10 +87,13 @@ class Visualizer:
         return _chart_in_html(fig)
 
     # 3 ==========================================================================
-    # Load the world map colored according count of rows in each country
+    
     @staticmethod
     def plot_world_map(grouped_df, type_of_netflix):
-        
+
+        # Plot a world map colored according to the count of rows in each country.
+        # Returns the HTML content of the Plotly figure.
+
         custom_color_scale = ['#ffb3b3','#ff9999', '#ff8080','#ff6666', '#ff4d4d','#ff3333', '#ff0000', '#cc0000', '#990000']
 
         fig = px.choropleth(
@@ -102,7 +113,7 @@ class Visualizer:
             visible=True,
             showcountries=True,
             countrywidth=1,  
-            countrycolor='grey'  # Set the color of country borders
+            countrycolor='grey' 
         )
 
         fig.update_layout(
@@ -115,25 +126,28 @@ class Visualizer:
         return _chart_in_html(fig)
 
     # 4 ===============================================================
-    # Scatter plot of age group by country
+    
     @staticmethod
     def scatter_plot_of_age_group(grouped_df, type_of_netflix, total_count):
         
-        scatter_fig = px.scatter(grouped_df, x='country', y='age_group',  color='age_group', size='percentage',
-                                title = f"Percentage of Age Group by Country for {type_of_netflix} (Total Count: {total_count} and above)", 
-                                category_orders = {'age_group': ['Kids', 'Older Kids', 'Teens', 'Young Adults', 'Adults']},
-                                size_max = 35,
-                                color_discrete_map = {
-                                    'Kids': '#ffb3b3',         
-                                    'Older Kids': '#ff8080', 
-                                    'Teens': '#ff6666',     
-                                    'Young Adults': '#ff4d4d', 
-                                    'Adults': '#ff0000'       
-                                },
-                                hover_data = {'age_group': False, 'count': True, 'percentage': ':.2f%'} 
-                                )  
+        # Create a scatter plot of age groups by country.
+        # Returns the HTML content of the Plotly figure.
 
-        # Update layout to change background color and other elements
+        scatter_fig = px.scatter(
+            grouped_df, x='country', y='age_group',  color='age_group', size='percentage',
+            title = f"Percentage of Age Group by Country for {type_of_netflix} (Total Count: {total_count} and above)", 
+            category_orders = {'age_group': ['Kids', 'Older Kids', 'Teens', 'Young Adults', 'Adults']},
+            size_max = 35,
+            color_discrete_map = {
+                'Kids': '#ffb3b3',         
+                'Older Kids': '#ff8080', 
+                'Teens': '#ff6666',     
+                'Young Adults': '#ff4d4d', 
+                'Adults': '#ff0000'       
+            },
+            hover_data = {'age_group': False, 'count': True, 'percentage': ':.2f%'} 
+        )  
+
         scatter_fig.update_layout(
             plot_bgcolor = 'black',  paper_bgcolor = 'black',  
             font = dict(color='white'),  
@@ -147,4 +161,3 @@ class Visualizer:
 
         return _chart_in_html(scatter_fig)
 
-    # =======================================================================
